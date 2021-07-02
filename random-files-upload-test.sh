d5854b96c78029597db92e0aaa08428906901714
Southside <293741+willief@users.noreply.github.com>
2021-07-02 21:22:46 +0100

    Update random-files-upload-test.sh
    Tidy up commentd lines and improve wording.

--- random-files-upload-test.sh
+++ random-files-upload-test.sh
@@ -16,9 +16,6 @@
 #    append measured time to the end of log file
 #   wait D milliseconds
 
-#upload files to SAFE Network and check time elapsed
-#upload checksums for each file to ensure we are dealing with standardised test files
-
 #Arguments
 INPUT_S=$1
 INPUT_N=$2
@@ -31,7 +28,6 @@ if [ $# -ne 3 ];
     echo " 15 iterations with a delay of 30 seconds between each run."
     exit 1
 fi    
-
 
 #Setup
 DEST_DIR=/home/$USER/tmp/testnet-file-uploads-$(date +%Y%m%d_%H%M)
@@ -46,32 +42,12 @@ mkdir -p $DEST_DIR && cd $DEST_DIR
 safe keys create --test-coins --for-cli --preload 10000
 
 
-
-#get parameters
 echo "======================================================================================================="
 echo ""
-echo "This test will upload random data of size S a total of N times with a delay of D between each test run"
-#read -p "What size of file do you want to test with?   Units in kb " INPUT_S
-echo " The test file will be "$INPUT_S" kb."
-echo ""
-echo ""
-# TODO    input validation, default size in, no input case
-
-#read -p "How many times should we repeat this test?    " INPUT_N
-echo "The test will be repeated "$INPUT_N" times."
-# TODO    input validation
-#period of time to wait before repeating the test runs
-echo ""
-echo ""
-#read -p "How many seconds should we wait between test runs?     " INPUT_D
-echo "The test run will be repeated after a delay of "$INPUT_D" seconds."
-# TODO    input validation
+echo "This test will upload random data of size $INPUT_S" kb for a total of "$INPUT_N" iterations with a delay of "INPUT_D" seconds between iterations."
 
 SAFE_VERSION=`safe -V`
 NODE_VERSION=`safe node bin-version`
-
-
-echo "========  SAFE Network testnet PUT files test  ====================================================="
 echo ""
 echo "          output files will be written to " $DEST_DIR
 echo ""
@@ -80,10 +56,7 @@ echo "          "$NODE_VERSION
 #echo "         "$AUTH_VERSION
 echo ""
 echo ""
-
-echo "Before starting tests, check there is sufficient balance in the account to be used"
 OPEN_BALANCE=`safe keys balance --json|tail -n1|cut -f2 -d':'`
-echo ""
 echo "This account has a balance of "$OPEN_BALANCE
 echo ""
 echo "==============================================================="
@@ -93,10 +66,9 @@ while [ $loop -le $INPUT_N ]
 do
     dd if=/dev/urandom of=$loop_random$INPUT_S.dat bs=1024 count=$INPUT_S >/dev/null
     (time safe files put $DEST_DIR/$INPUT_S.dat) |& tee -a -i $INPUT_N-data.txt
-    #time safe files put $DEST_DIR/$INPUT_S.dat |tee -a -i $INPUT_N-data.txt
     sleep $INPUT_D
-    echo "sleeping for "$INPUT_D" seconds"
-    rm -v $DEST_DIR/$INPUT_S.dat
+    echo "sleeping for "$INPUT_D" seconds until the next iteration"
+    rm  $DEST_DIR/$INPUT_S.dat
     ((loop++))           
 done
 
