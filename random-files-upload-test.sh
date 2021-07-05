314283d2393271325cb45aef4dcd9dbf3844ae4f
Southside <293741+willief@users.noreply.github.com>
2021-07-05 02:22:01 +0100

    Change logging destination, sort JSON formatting

--- random-files-upload-test.sh
+++ random-files-upload-test.sh
@@ -30,30 +30,28 @@ if [ $# -ne 3 ];
 fi    
 
 #Setup
-DEST_DIR=/home/$USER/tmp/testnet-file-uploads-$(date +%Y%m%d_%H%M)
-if [[ -d  $DEST_DIR ]]
+DEST_DIR=/home/$USER/tmp/upload-tests
+if [ ! -d  $DEST_DIR ];
     then
-    rm -rf $DEST_DIR
+    mkdir -p $DEST_DIR && cd $DEST_DIR
 fi
-mkdir -p $DEST_DIR && cd $DEST_DIR
-
 
 SAFE_VERSION=`safe -V`
 NODE_VERSION=`safe node bin-version`
 SAFE_COMMAND=put
 #safe setup
 
-#safe keys create --test-coins --for-cli --preload 10000
+safe keys create --test-coins --for-cli --preload 10000 > /dev/null
 
 #logging setup
-touch output.json
+
+touch results-$(date +%Y%m%d_%H%M) && ln -s  results-$(date +%Y%m%d_%H%M) output.json
 echo '{"testrun":"' `date`'",' \
     '"safe_version": "'$SAFE_VERSION'", "node_version":"'$NODE_VERSION'", "safe_command":"'$SAFE_COMMAND'",'\
      '"filesize":"'$FILESIZE'", "no_of_iterations": "'$ITERATIONS'", "delay": "'$DELAY'" , "data":[{' >>output.json
 echo "======================================================================================================="
 echo ""
 echo "This test will upload random data of size "$FILESIZE" kb for a total of "$ITERATIONS" iterations with a delay of "$DELAY" seconds between iterations."
-
 echo ""
 echo "          output files will be written to " $DEST_DIR
 echo ""
@@ -65,30 +63,35 @@ echo ""
 
 # ################ Next two lines commented out for compatabilty with sn_node0.7.n ###############
 
-#OPEN_BALANCE=`safe keys balance --json|tail -n1|cut -f2 -d':'`
-#echo "This account has a balance of "$OPEN_BALANCE
+OPEN_BALANCE=`safe keys balance --json|tail -n1|cut -f2 -d':'`
+echo "This account has a balance of "$OPEN_BALANCE
 
 # #################################################################################################
 echo ""
 echo "==============================================================="
+# run tests
 
 loop=1
 while [ $loop -le $ITERATIONS ]
 do
-    echo '"iteration": "'$loop'", "elapsed time": " ' >> output.json
-    dd if=/dev/urandom of=$FILESIZE.dat bs=1024 count=$FILESIZE >/dev/null
-   /usr/bin/time -a -o output.json -f "\t%e" safe files $SAFE_COMMAND $DEST_DIR/$FILESIZE.dat > /dev/null 
-    
+    echo '"iteration": "'$loop'", ' >> output.json
+    dd if=/dev/urandom of=$DEST_DIR/test.dat bs=1024 count=$FILESIZE > /dev/null
+    /usr/bin/time -a -o output.json -f '"elapsed time":"\t%e","user time":"\t%U","system time": "\t%S"}' safe files $SAFE_COMMAND $DEST_DIR/test.dat > /dev/null
     sleep $DELAY
     echo "sleeping for "$DELAY" seconds until the next iteration"
-    #rm  $DEST_DIR/$FILESIZE.dat
+    rm  $DEST_DIR/test.dat
     ((loop++))           
 done
 
  #CLOSE_BALANCE=`safe keys balance|tail -n1|cut -f2 -d':'`
 
-echo '"}' >> output.json
+echo ']}' >> output.json
 echo  "    Test complete for " $ITERATIONS " runs of file size " $FILESIZE "kb with delay of " $DELAY " seconds between each run"
 echo ""
 echo "=========================================================================================================================="
+
+#clean up
+rm output.json
+
 exit 0
+
